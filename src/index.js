9a7d91a225b5eb9eb0b8354d104b44ce597b90f8
Santinolopez56 <ss.lopez@alumno.etec.um.edu.ar>
2024-05-09 11:23:01 -0300

    error de mierda

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-import express from "express";
+import express from 'express'
+import { createPool} from 'mysql2/promise'
 
 const app = express()
+
+const pool = createPool({
+    host:'localhost',
+    user:'root',
+    password:'123456',
+    port: 3307,
+})
 
 app.get('/',(req,res) => {
     res.send ("Andando")
 })   
+
+app.get('/ping', async (req,res) => {
+    const result = await pool.query ('Select Now()')
+    res.json (result[0])
+})
 
 app.listen(3000)
 console.log('Server on Port',3000)
