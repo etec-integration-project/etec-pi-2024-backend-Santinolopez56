c273b71d25cd8dd4e807c294d8a958d5f59b1875
Santinolopez56 <ss.lopez@alumno.etec.um.edu.ar>
2024-09-12 10:20:10 -0300

    Seguir

--- src/utils/crearProducts.js
+++ src/utils/crearProducts.js
@@ -4,10 +4,15 @@ export async function crearProductos(pool) {
         await pool.query(`
             INSERT INTO products (name, price) 
             VALUES 
-            ('', 200),
-            ('Sillon 2', 400),
-            ('Sillon 3', 500),
-            ('Sillon 4', 700);
+            ('Imanes Circuitos', 100),
+            ('Gorra Ferrari', 300),
+            ('Buzo Mercedes', 400),
+            ('Remera Aston Martin', 500),
+            ('Remera Ferrari', 600),
+            ('Remera Red Bull', 700),
+            ('Campera McLaren', 800),
+            ('Llavero Ruedas', 200),
+            ('Auto de Juguete Mercedes', 900);
         `);
 
         console.log('Productos insertados correctamente.');
